50e89b8b7346089a5b24de54888c84e769ef56ad
damvantaineos <taidv@neoscorp.vn>
2018-08-27 16:24:14 +0700

    false predicted age and gender

--- src_new/main_tracking_predict_age_yu4u.py
+++ src_new/main_tracking_predict_age_yu4u.py
@@ -14,18 +14,21 @@ from model import Generator
 from PIL import Image
 from tracker import Tracker
 from wide_resnet import WideResNet 
+from torch.autograd import Variable
+from torchvision.transforms import ToTensor, ToPILImage
+import torch
 
 ix, iy, ex, ey = -1, -1, -1, -1
 ix1, iy1, ix2, iy2 = -1, -1, -1, -1
 cap_from_stream = False
 
-path = '/home/neosai/Documents/dataset/camera_supervisor/1_02.avi'
+path = '/home/neosai/Documents/dataset/camera_supervisor/1_03.avi'
 
-def limit_mem():
-	K.get_session().close()
-	cfg = K.tf.ConfigProto()
-	cfg.gpu_options.allow_growth = True
-	K.set_session(K.tf.Session(config=cfg))
+# def limit_mem():
+# 	K.get_session().close()
+# 	cfg = K.tf.ConfigProto()
+# 	cfg.gpu_options.allow_growth = True
+# 	K.set_session(K.tf.Session(config=cfg))
 
 def draw_rec(event, x, y, flags, param):
 	global ix, iy, ex, ey, drawing, mode
@@ -34,6 +37,19 @@ def draw_rec(event, x, y, flags, param):
 	elif event == cv2.EVENT_LBUTTONUP:
 		ex, ey = x, y
 		cv2.rectangle(param, (ix, iy), (x, y), (0, 255, 0), 0)
+
+def draw_line(event, x, y, flags, param):
+	global ix1, iy1, ix2, iy2, drawing, mode
+	if event == cv2.EVENT_LBUTTONDOWN:
+		ix1, iy1 = x, y
+		print(ix1, iy1)
+
+	elif event == cv2.EVENT_LBUTTONUP:
+		ix2, iy2 = x, y
+		print(ix2, iy2)
+		# cv2.line(param, (ix1, iy1), (x, y), (0, 0, 255), 2)
+		cv2.circle(param, (ix1, iy1), 5, (0, 0, 255), 4)
+		# cv2.circle(param, (x, y), 5, (0, 0, 255), 2)
 
 def get_crop_size(video_path):
 	cap = cv2.VideoCapture(video_path)
@@ -53,6 +69,23 @@ def get_crop_size(video_path):
 				break
 		break
 
+def get_crop_size1(video_path):
+	cap = cv2.VideoCapture(video_path)
+	while cap.isOpened():
+		ret, frame = cap.read()
+		if cap_from_stream:
+			frame = cv2.resize(frame, (1280, 720))
+		cv2.namedWindow('draw_line')
+		cv2.setMouseCallback('draw_line', draw_line, frame)
+		print("Choose your point of interest!")
+		while 1:
+			cv2.imshow('draw_line', frame)
+			k = cv2.waitKey(1) & 0xFF
+			if k == ord('a'):
+				cv2.destroyAllWindows()
+				break
+		break
+
 def convert_bbox(bbox):
 	a0 = int(bbox[0][0])
 	a1 = int(bbox[1][0])
@@ -61,17 +94,33 @@ def convert_bbox(bbox):
 
 	return a0, a1, a2, a3
 
+def super_resolution_image(image, model):
+	image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
+	# image = image.cuda()
+	out = model(image)
+	out_img = ToPILImage()(out[0].data.cpu())
+	return out_img
+
 def main():
-	limit_mem()
+	tf.reset_default_graph() 
+	sess = tf.InteractiveSession()
+	sess = tf.Session()
 	aligned_images = []
 	upscale_factor = 4
+	img_size = 64
+	depth = 16
+	k = 8
+	weight_file = "/home/neosai/Documents/github/age-gender-estimation/utkface/weights.29-3.76_utk.hdf5"
+	model_predict_age_and_gender = WideResNet(img_size, depth=depth, k=k)()
+	model_predict_age_and_gender.load_weights(weight_file)
 	model_name = "/home/neosai/Documents/projects/deep_face_recognition/weights/netG_epoch_4_100.pth"
 	model = Generator(upscale_factor).eval()
 	model.load_state_dict(torch.load(model_name, map_location=lambda storage, loc: storage))
 	get_crop_size(path)
+	get_crop_size1(path)
 	print('Your area of interest: ', ix, ' ', iy, ' ', ex, ' ', ey)
 	area = (ix, iy, ex, ey)
-
+	print("iy1", iy1)
 	# Create opencv video capture object
 	cap = cv2.VideoCapture(path)
 	w = int(cap.get(3))
@@ -81,7 +130,7 @@ def main():
 		h = 720
 	# fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	out = cv2.VideoWriter('/home/neosai/Documents/projects/deep_face_recognition/video/1_024.avi', fourcc, 15, (w, h))
+	out = cv2.VideoWriter('/home/neosai/Documents/projects/deep_face_recognition/video/1_034.avi', fourcc, 15, (w, h))
 
 	# Create Object Detector
 	detector = YOLO()
@@ -94,20 +143,19 @@ def main():
 	count_people = {'people_come_in': 0, 'people_come_out': 0}
 	id_number = 0
 	img_size = 64
-	depth = 16
-	k = 8
-	weight_file = "/home/neosai/Documents/github/age-gender-estimation/utkface/weights.29-3.76_utk.hdf5"
-	model_predict_age_and_gender = WideResNet(img_size, depth=depth, k=k)()
-	model_predict_age_and_gender.load_weights(weight_file)
-
+	font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+	# depth = 16
+	# k = 8
+	# weight_file = "/home/neosai/Documents/github/age-gender-estimation/utkface/weights.29-3.76_utk.hdf5"
+	# model_predict_age_and_gender = WideResNet(img_size, depth=depth, k=k)()
+	# model_predict_age_and_gender.load_weights(weight_file)
+	faces = np.empty((1, 64, 64, 3))
 	while cap.isOpened():
 		# Capture frame-by-frame
 		ret, frame = cap.read()
-
 		if cap_from_stream:
 			frame = cv2.resize(frame, (1280, 720))
 		frame = Image.fromarray(frame)
-
 		# Detect and return centeroids of the objects in the frame
 		result, centers, box_detected, obj_type = detector.detect_image(frame, area)
 		result = np.asarray(result)
@@ -117,39 +165,40 @@ def main():
 		for bbox in box_detected:
 			a0, a1, a2, a3 = bbox[0], bbox[1], bbox[2], bbox[3]
 			cv2.rectangle(result, (a0 - 10, a1 - 10), (a2 + 10, a3 + 10), (0, 255, 0), 3)
-			if a1 < iy1 + 50 and a1 > iy1 -50:
-				print(a1)
-				image_crop = frame[a1:a3, a0:a2]
-				# cv2.imwrite("image.jpg", image_crop)
-				image_crop = Image.fromarray(image_crop, 'RGB')
-				image_crop = super_resolution_image(image_crop, model)
-				image_crop_array = np.asarray(image_crop)
+			print(a1)
+			print(iy1)
+			# if a1 < iy1 + 50 and a1 > iy1 -50:
+			image_crop = frame[a1:a3, a0:a2]
+			cv2.imwrite("image.jpg", image_crop)
+			image_crop = Image.fromarray(image_crop, 'RGB')
+			image_crop = super_resolution_image(image_crop, model)
+			image_crop_array = np.asarray(image_crop)
 
-				face_male_resize = image_crop.resize((img_size, img_size), Image.ANTIALIAS)
-				face = np.array(face_male_resize)
-				aligned_images.append(face)
-				# faces[0, :, :, :] = face
-				# age_predict, gender_predict = sess.run([age, gender], feed_dict={images_pl: aligned_images, train_mode: False})
-				# aligned_images = []
+			face_male_resize = image_crop.resize((img_size, img_size), Image.ANTIALIAS)
+			face = np.array(face_male_resize)
+			aligned_images.append(face)
+			faces[0, :, :, :] = face
+			# age_predict, gender_predict = sess.run([age, gender], feed_dict={images_pl: aligned_images, train_mode: False})
+			
 
-				# predict ages and genders of the detected faces
-				results = model_predict_age_and_gender.predict(aligned_images)
-				predicted_genders = results[0]
-				ages = np.arange(0, 101).reshape(101, 1)
-				predicted_ages = results[1].dot(ages).flatten()
-
-				label = "{}, {}".format(int(predicted_ages[0]),
-										"F" if predicted_genders[0][0] > 0.5 else "M")
-				# print(gender_predict)
-				# print(type(gender_predict))
-				# label = "{}, {}".format(int(age_predict[0]), "Female" if gender_predict[0] == 0 else "Male")
-				id_number += 1
-				print(label)
-				name = "../image/102/id_{}, {}".format(id_number, label)
-				cv2.imwrite(name + ".jpg", image_crop_array)
-				cv2.rectangle(result, (a0 - 5, a1 - 5), (a2 + 5, a3 + 5), color=(0, 0, 255),
-							  thickness=3)
-				cv2.putText(result, label, (a0 + 6, a1 - 6), font, 2, (0, 255, 0), 3, cv2.LINE_AA)
+			# predict ages and genders of the detected faces
+			results = model_predict_age_and_gender.predict(faces)
+			predicted_genders = results[0]
+			ages = np.arange(0, 101).reshape(101, 1)
+			predicted_ages = results[1].dot(ages).flatten()
+			aligned_images = []
+			label = "{}, {}".format(int(predicted_ages[0]),
+									"F" if predicted_genders[0][0] > 0.5 else "M")
+			# print(gender_predict)
+			# print(type(gender_predict))
+			# label = "{}, {}".format(int(age_predict[0]), "Female" if gender_predict[0] == 0 else "Male")
+			id_number += 1
+			print(label)
+			name = "../image/102/id_{}, {}".format(id_number, label)
+			cv2.imwrite(name + ".jpg", image_crop_array)
+			cv2.rectangle(result, (a0 - 5, a1 - 5), (a2 + 5, a3 + 5), color=(0, 0, 255),
+						  thickness=3)
+			cv2.putText(result, label, (a0 + 6, a1 - 6), font, 2, (0, 255, 0), 3, cv2.LINE_AA)
 
 		#####
 
